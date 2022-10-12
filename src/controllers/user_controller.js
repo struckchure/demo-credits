e417da3598ea28de88bcfd914738b119cced8aaf
Mohammed Al Ameen <ameenmohammed2311@gmail.com>
2022-10-12 23:18:04 +0100

    feat(src/controllers): adds contollers

---
+++ src/controllers/user_controller.js
@@ -0,0 +1,29 @@
+const userService = require("../service/user_service");
+
+class UserController {
+  async register(req, res) {
+    try {
+      const user = await userService.createUser(req.body);
+      res.status(201).json({ data: user });
+    } catch (err) {
+      res.status(400).json({ data: null, error: err.message });
+    }
+  }
+
+  async login(req, res) {
+    try {
+      const user = await userService.authenticateUser(req.body);
+      if (user) {
+        res.status(200).json({ data: user });
+      } else {
+        res
+          .status(400)
+          .json({ data: null, error: "Invalid username or password" });
+      }
+    } catch (err) {
+      res.status(400).json({ data: null, error: err.message });
+    }
+  }
+}
+
+module.exports = new UserController();
