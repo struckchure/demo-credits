4d2f197c546f9a84d136f99dc88e20f3a12712fa
Mohammed Al Ameen <ameenmohammed2311@gmail.com>
2022-10-16 08:57:04 +0100

    build(knexfile): adds test configurations to config

--- knexfile.js
+++ knexfile.js
@@ -6,18 +6,26 @@
 
 require("dotenv/config");
 
-const { MYSQL_HOST, MYSQL_PORT, MYSQL_USER, MYSQL_PASSWORD, MYSQL_DATABASE } =
-  process.env;
+const {
+  // test database
+  MYSQL_TEST_DATABASE,
+  // development database
+  MYSQL_HOST,
+  MYSQL_PORT,
+  MYSQL_USER,
+  MYSQL_PASSWORD,
+  MYSQL_DATABASE,
+} = process.env;
 
 module.exports = {
-  development: {
+  test: {
     client: "mysql2",
     connection: {
       host: MYSQL_HOST,
       port: MYSQL_PORT,
       user: MYSQL_USER,
       password: MYSQL_PASSWORD,
-      database: MYSQL_DATABASE,
+      database: MYSQL_TEST_DATABASE,
     },
     pool: {
       min: 2,
@@ -29,7 +37,7 @@ module.exports = {
     },
   },
 
-  staging: {
+  development: {
     client: "mysql2",
     connection: {
       host: MYSQL_HOST,
