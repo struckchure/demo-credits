07bbae769fdb2000d082bad422d8d02da69a67fd
Mohammed Al Ameen <ameenmohammed2311@gmail.com>
2022-10-18 12:24:26 +0100

    feat(src/db/utils): adds function to truncate database

--- src/db/utils.js
+++ src/db/utils.js
@@ -1,21 +1,35 @@
 const db = require(".");
 
 async function migrateDatabase() {
-  await db.migrate.latest({ directory: "./src/db/migrations" });
+  try {
+    await db.migrate.latest({ directory: "./src/db/migrations" });
+  } catch (error) {
+    console.log(error.message);
+  }
 }
 
-async function rollbackDatabase() {
-  // remove foreign key constraints
-  await db.raw("SET FOREIGN_KEY_CHECKS = 0");
+async function truncateDatabase() {
+  try {
+    // remove foreign key constraints
+    await db.raw("SET FOREIGN_KEY_CHECKS = 0");
 
-  await db.migrate.rollback({ directory: "./src/db/migrations" });
+    await db("users").truncate();
+    await db("wallets").truncate();
+    await db("transactions").truncate();
 
-  // restore foreign key constraints
-  await db.raw("SET FOREIGN_KEY_CHECKS = 1");
+    // restore foreign key constraints
+    await db.raw("SET FOREIGN_KEY_CHECKS = 1");
+  } catch (error) {
+    console.log(error.message);
+  }
 }
 
 async function destroyDatabase() {
-  await db.destroy();
+  try {
+    await db.destroy();
+  } catch (error) {
+    console.log(error.message);
+  }
 }
 
-module.exports = { migrateDatabase, rollbackDatabase, destroyDatabase };
+module.exports = { migrateDatabase, truncateDatabase, destroyDatabase };
